d9aadb0dc8fd609a6b72237f5e3e5ced608a66dd
Bob Clewell <bobclewell@gmail.com>
2014-06-09 11:22:13 -0400

    Changed wsgi.py path to my local DM repo.

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,9 @@ import os
 import sys
 
 # Change this path to the absolute location of your DM repository
-sys.path.append('/home/tandres/dm_live')
+sys.path.append('/Users/bobclewell/Code/DM/ALPHA/DM')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
